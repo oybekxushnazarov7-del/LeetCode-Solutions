094ea5ff05640cedd79d4a752d2c0cf24ab8ebfe
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-03-22 21:50:32 +0500

    Time: 2 ms (50.82%), Space: 8.5 MB (65.09%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0) return false;
+        int original=x;
+        long long sum=0;
+        while(x>0){
+            int n = x%10;
+            sum=sum*10+n;
+            x=x/10;
+        }
+        return sum==original;
+    }
+};
