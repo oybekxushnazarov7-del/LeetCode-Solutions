d28fe742dd236299dc23e0aeb6cc0bd1b747a4a8
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-03-22 21:35:01 +0500

    Time: 0 ms (100%), Space: 24.5 MB (47.63%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int sum=0;
+        for(int i=0;i<hours.size();i++){
+            if(hours[i]>=target) sum++;
+        }
+        return sum;
+    }
+};
