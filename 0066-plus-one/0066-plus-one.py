49003399b808dc71acd75a12fcd60ae993cb171a
oybek xushnazarov <oybekxushnazarov7@gmail.com>
2026-07-31 17:05:11 +0500

    Time: 0 ms (100%), Space: 19.5 MB (18.25%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,4 +1,15 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        return list(map(int, list(str(int(''.join(map(str, digits)))+1))))
+        # return list(map(int, list(str(int(''.join(map(str, digits)))+1))))
+        res = 0
+        for i, v in enumerate(digits[::-1]):
+            res+=(10**i * v)
+        res+=1
+        lst = []
+        while res > 0:
+            j = res%10
+            res//=10
+            lst = [j, *lst]
+        return lst
+
             
